ea19f0d83dcb4aec383365c84f08a21d4a8a67a1
AndrewSJeffrey <AJeffrey@fleetsmart.co.uk>
2015-06-21 21:17:23 +0100

    Added idea, got hibernate working with controllers!

--- src/main/java/com/eddie/common/controller/UserController.java
+++ src/main/java/com/eddie/common/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.eddie.common.controller;
 
+import com.eddie.LiveChat;
 import com.eddie.common.model.User;
+import com.eddie.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @Controller
@@ -11,13 +15,19 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
 
-
+    @Autowired
+    private UserDao userDao;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public
     @ResponseBody
     User getUser(@RequestParam(value = "user") String username, @RequestParam(value = "password") String password) {
 
+        List<com.eddie.domain.User> list = userDao.list();
+        for (com.eddie.domain.User user : list) {
+            System.out.println(user);
+        }
+        LiveChat chat = new LiveChat();
 
         if ("sysadmin".equalsIgnoreCase(username)) {
             User user = new User();
